6e2bd06dc4492363ec3c9ca3389879d510cd52e4
codakcodak <tjwjdgus83@naver.com>
2022-04-21 15:41:51 +0900

    docs: create form of model layer

---
+++ back/src/db/models/Music.js
@@ -0,0 +1,21 @@
+import {MusicModel} from "../schemas/music"
+
+class Music{
+    static async create({}){
+
+    }
+
+    static async findById({settings}){
+
+    }
+
+    static async findByFilter({settings}){
+
+    }
+
+    static async findAll(){
+
+    }
+}
+
+export {Music}
