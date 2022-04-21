f1c871c7c789bdf3cee10b84b21f064a37104378
codakcodak <tjwjdgus83@naver.com>
2022-04-21 16:56:12 +0900

    feat:#60 add two api
    get all musics and get musics by filter

---
+++ back/src/routers/musicRouter.js
@@ -0,0 +1,29 @@
+import { Router } from "express";
+
+const musicRouter=Router()
+
+musicRouter.get("/musics",
+    async function(req,res,next){
+        try{
+            const musics=await musicService.getMusics()
+            res.status(200).json(musics)
+        } catch(error){
+            next(error)
+        }
+    }
+)
+
+musicRouter.get("/musics/by",
+    async function(req,res,next){
+        try{
+            filter=req.body.filter
+            mode=req.body.mode
+            const filteredMusics=await musicService.getMusicsBy({filter,mode})
+            res.status(200).json(filteredMusics)
+        }catch(error){
+            res.status(400)
+        }
+    }
+)
+
+export {musicRouter}
