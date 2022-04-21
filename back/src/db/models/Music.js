3c06f3967b0b4cc4cf5855a379819743b82f2f66
codakcodak <tjwjdgus83@naver.com>
2022-04-21 16:18:39 +0900

    feat: #59 create Model layer
    added findByFilter,findById,findAll functions

--- back/src/db/models/Music.js
+++ back/src/db/models/Music.js
@@ -1,16 +1,31 @@
 import {MusicModel} from "../schemas/music"
 
 class Music{
-    static async findById({settings}){
-
+    static async findById({music_id}){
+        const music=await MusicModel.findOne({id:music_id})
+        return music
     }
 
-    static async findByFilter({settings}){
+    static async findByFilter({filters}){
+        [minTempo,maxTempo]=filters.Tempo
+        [minDanceability,maxDanceability]=filters.Danceability
+        [minYear,maxYear]=filters.Year
 
+        const filteredMusics=await MusicModel.find(
+            { $and :
+                [
+                    {"tempo":{$gte : minTempo,$lte :maxTempo}},
+                    {"danceability":{$gte : minDanceability,$lte:maxDanceability}},
+                    {"year":{$gte:minYear,$lte:minYear}}
+                ]
+            }
+        )
+        return filteredMusics
     }
 
     static async findAll(){
-
+        const musics=await MusicModel.find({})
+        return musics
     }
 }
 
