6b8c5b45a37ab24e7fea3f034fa4feac9bd378dd
codakcodak <tjwjdgus83@naver.com>
2022-04-21 15:34:09 +0900

    docs: create Musicschema

---
+++ back/src/db/schemas/music.js
@@ -0,0 +1,42 @@
+import {Schema,model} from "mongoos"
+
+const MusicSchema=new Schema(
+    {
+        id:{
+            type:String,
+            required:true
+        },
+        title:{
+            type:String,
+            required:true
+        },
+        artists:{
+            type:String,
+            required:true
+        },
+        artists_ids:{
+            type:String,
+            required:true
+        },
+        danceability:{
+            type:Number,
+            required:true
+        },
+        tempo:{
+            type:Number,
+            required:true
+        },
+        year:{
+            type:Number,
+            required:true
+        },
+        energy:{
+            type:Number,
+            required:true
+        }
+    }
+)
+const MusicModel=model("Music",MusicSchema)
+
+export {MusicModel}
+
