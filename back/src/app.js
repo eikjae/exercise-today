80c2dd540b782a6d8522c272522f8bc3d05dca42
codakcodak <tjwjdgus83@naver.com>
2022-04-22 13:59:09 +0900

    docs: add musicRouter in app.js

--- back/src/app.js
+++ back/src/app.js
@@ -2,6 +2,7 @@ import cors from "cors";
 import express from "express";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 import { userAuthRouter } from "./routers/userRouter";
+import {musicRouter} from "./routers/musicRouter"
 
 const app = express();
 
@@ -20,6 +21,7 @@ app.get("/", (req, res) => {
 });
 
 // router, service 구현 (userAuthRouter는 맨 위에 있어야 함.)
+app.user(musicRouter)
 app.use(userAuthRouter);
 
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
