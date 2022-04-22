97af652768178488190af4023b23bd2287921230
[4기-1반] 이예원 <panggun503@gmail.com>
2022-04-22 16:39:05 +0900

    feat: 음식 카테고리 UI 수정 및 계산 결과 창 구성

--- front/src/components/main/BadgeVisibility.js
+++ front/src/components/main/BadgeVisibility.js
@@ -1,4 +1,5 @@
-import * as React from "react";
+import React, { useState } from "react";
+import * as Api from "../../api";
 import Box from "@mui/material/Box";
 import Badge from "@mui/material/Badge";
 import ButtonGroup from "@mui/material/ButtonGroup";
@@ -6,45 +7,75 @@ import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 import RestaurantIcon from "@mui/icons-material/Restaurant";
+import styled from "styled-components";
 
-export default function BadgeVisibility({ food }) {
-  const [count, setCount] = React.useState(0);
+const StyledWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  /* width: 50%; */
+  min-width: 270px;
+  height: 60px;
+  background-color: #e7f0ff;
+  border-radius: 10px;
+  margin-left: 8px;
+  margin-right: 8px;
+  margin-bottom: 30px;
+`;
+
+const FoodLabel = styled.label`
+  margin-left: 14.5px;
+  font-size: 15px;
+  font-weight: bold;
+  width: 90px;
+  position: absolute;
+  margin-top: 5.5px;
+  text-align: center;
+`;
+
+const FoodBadge = styled(Badge)`
+  margin-left: 10px;
+`;
+
+export default function BadgeVisibility({ food, setFoodInfo }) {
+  const [count, setCount] = useState(0);
+
   return (
-    <Box
-      sx={{
-        color: "action.active",
-        display: "flex",
-        flexDirection: "column",
-        "& .MuiBadge-root": {
-          marginRight: 2,
-        },
-      }}
-    >
-      <div style={{ marginBottom: "20px", marginRight: "20px" }}>
-        <label style={{ fontSize: "15px" }}>{food}</label>
-        &nbsp;
-        <Badge color="primary" badgeContent={count}>
-          <RestaurantIcon />
-        </Badge>
-        <ButtonGroup>
-          <Button
-            aria-label="reduce"
-            onClick={() => {
-              setCount(Math.max(count - 1, 0));
-            }}
-          >
-            <RemoveIcon fontSize="small" />
-          </Button>
-          <Button
-            aria-label="increase"
-            onClick={() => {
-              setCount(count + 1);
-            }}
-          >
-            <AddIcon fontSize="small" />
-          </Button>
-        </ButtonGroup>
-      </div>
-    </Box>
+    <StyledWrapper>
+      <Box
+        sx={{
+          color: "action.active",
+          display: "flex",
+          flexDirection: "column",
+          "& .MuiBadge-root": {
+            marginRight: 2,
+          },
+        }}
+      >
+        <div>
+          <FoodBadge color="primary" badgeContent={count}>
+            <RestaurantIcon />
+          </FoodBadge>
+          <ButtonGroup>
+            <Button
+              aria-label="reduce"
+              onClick={() => {
+                setCount(Math.max(count - 1, 0));
+              }}
+            >
+              <RemoveIcon fontSize="small" />
+            </Button>
+            <Button
+              aria-label="increase"
+              onClick={() => {
+                setCount(count + 1);
+              }}
+            >
+              <AddIcon fontSize="small" />
+            </Button>
+          </ButtonGroup>
+          <FoodLabel>{food}</FoodLabel>
+        </div>
+      </Box>
+    </StyledWrapper>
   );
 }
--- front/src/components/main/MainPage.js
+++ front/src/components/main/MainPage.js
@@ -4,58 +4,76 @@ import { Container, Grid } from "@mui/material";
 import styled from "styled-components";
 import BadgeVisibility from "./BadgeVisibility";
 
+const StyledContainer = styled(Container)`
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  margin-top: 100px;
+  margin-bottom: 70px;
+`;
+
+const FoodWrapper = styled.div`
+  display: flex;
+  justify-content: start;
+  flex-wrap: wrap;
+`;
+
+const CalorieWrapper = styled.div`
+  margin-top: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  width: 50%;
+  height: 100px;
+  background-color: #61ac77;
+  border-radius: 10px;
+`;
+
+const CalorieResult = styled.h2`
+  margin-right: 30px;
+  font-weight: bold;
+`;
+
 export default function MainPage() {
-  const StyledContainer = styled(Container)`
-    max-width: 1000px;
-    height: 1000px;
-    background-color: white;
-    ${({ theme }) => theme.tablet`
-    background-color: blue;
-  `}
-    ${({ theme }) => theme.mobile`
-    background-color: red;
-  `}
-  border: 3px solid white;
-    /* height: 100%; */
-  `;
-
-  const Title = styled.h1`
-    text-align: center;
-    margin-top: 100px;
-    margin-bottom: 50px;
-  `;
-
   const [foods, setFoods] = useState([]);
+  const [foodInfo, setFoodInfo] = useState([]);
+  const [calories, setCalories] = useState(0);
 
   useEffect(() => {
     const fetch = async () => {
       try {
         const res = await Api.get("foods");
-        console.log(res.data);
-        // setFoods()
+        setFoods(res.data);
       } catch (err) {
         console.error(err);
       }
     };
     fetch();
-  }, [foods]);
+  }, []);
+
+  // + 버튼이 눌리면 일단 foodInfo에 정보를 넣음
+  // submit(계산) 버튼을 누를시 volumne이 0인 값은 배열 filter하여 제거하고 POST 요청
 
   return (
-    // 여러 props 적용 확인
     <StyledContainer>
       <header>
-        <Title>오늘 무엇을 드셨나요?</Title>
+        <Title>🍴 오늘 무엇을 드셨나요? 🍴</Title>
       </header>
-      {/* <BadgeVisibility key={1} food={food} /> */}
-      <Grid container spacing={3} justifyContent="center" alignItems="center">
+      <Grid container justifyContent="center" alignItems="center">
         {foods.map((food) => (
-          <Grid item>
-            <BadgeVisibility key={food} food={food} />
-          </Grid>
+          <FoodWrapper item key={food}>
+            <BadgeVisibility key={food} food={food} setFoodInfo={setFoodInfo} />
+          </FoodWrapper>
         ))}
       </Grid>
-      <br />
-      <h2 style={{ textAlign: "center" }}>(칼로리 결과)</h2>
+      <CalorieWrapper>
+        <CalorieResult>{calories}</CalorieResult>
+      </CalorieWrapper>
     </StyledContainer>
   );
 }
