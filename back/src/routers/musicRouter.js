6cc0f4e0d19238d27d3bd735ed6cfd6834abd710
codakcodak <tjwjdgus83@naver.com>
2022-04-23 12:27:42 +0900

    docs : #99 modified api

--- back/src/routers/musicRouter.js
+++ back/src/routers/musicRouter.js
@@ -1,33 +1,33 @@
-import { Router } from "express";
-import {musicService} from "../services/musicService"
-const musicRouter=Router()
+import { Router } from 'express';
+import { musicService } from '../services/musicService';
+const musicRouter = Router();
 
-musicRouter.get("/musics",
-    async function(req,res,next){
-        try{
-            const musics=await musicService.getMusics()
-            res.status(200).json(musics)
-        } catch(error){
-            next(error)
-        }
+musicRouter.get('/musics', async function (req, res, next) {
+    try {
+        const musics = await musicService.getMusics();
+        res.status(200).json(musics);
+    } catch (error) {
+        next(error);
     }
-)
+});
 
-musicRouter.post("/musics/by",
-    async function(req,res,next){
-        try{
-            const  filter=req.body.filter
-            const mode=req.body.mode
-            const limit=req.body.limit
-            const filteredMusics=await musicService.getMusicsBy({filter,mode,limit})
-            if (filteredMusics.errorMessage) {
-                throw new Error(filteredMusics.errorMessage);
-            }
-            res.status(200).json(filteredMusics)
-        }catch(error){
-            next(error)
+musicRouter.post('/musics/filtered/', async function (req, res, next) {
+    try {
+        const filter = req.body.filter;
+        const orderby = req.body.orderby;
+        const limit = req.body.limit;
+        const filteredMusics = await musicService.getMusicsBy({
+            filter,
+            orderby,
+            limit,
+        });
+        if (filteredMusics.errorMessage) {
+            throw new Error(filteredMusics.errorMessage);
         }
+        res.status(200).json(filteredMusics);
+    } catch (error) {
+        next(error);
     }
-)
+});
 
-export {musicRouter}
+export { musicRouter };
