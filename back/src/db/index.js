471a3382119551b089c0f40e3b32866d9309abf5
@eikjae <eikjae@gmail.com>
2022-04-23 18:53:54 +0900

    chore: index.js에 targetExercise 관련 import 추가

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { User } from "./models/User";
 import { Exercise } from "./models/Exercise";
+import { TargetExercise } from "./models/TargetExercise";
 
 const DB_URL =
   process.env.MONGODB_URL ||
@@ -16,4 +17,4 @@ db.on("error", (error) =>
   console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
 );
 
-export { User, Exercise };
+export { User, Exercise, TargetExercise };
