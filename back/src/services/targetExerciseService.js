3cd90080b67db04b73ac1d3a911af0d31441ffe4
@eikjae <eikjae@gmail.com>
2022-04-23 23:50:23 +0900

    chore: POST exercise/partandtool 관련 서비스 수정

--- back/src/services/targetExerciseService.js
+++ back/src/services/targetExerciseService.js
@@ -1,15 +1,20 @@
 import { TargetExercise } from "../db";
 
 class targetExerciseService {
-  static async fromPartAndTool({ bodyPart, equipment }) {
-    const targetExerciseList = await TargetExercise.findByPartAndTool({
+  static async fromPartAndTool({ bodyPart, equipment, target }) {
+    let targetExerciseList = await TargetExercise.findByPartAndTool({
       bodyPart,
       equipment,
+      target,
     });
 
     if (targetExerciseList == []) {
       const errorMessage = "부위와 장비 이름을 다시 한 번 확인해 주세요.";
       return { errorMessage };
+    }
+
+    if (targetExerciseList.length > 5) {
+      targetExerciseList = targetExerciseList.slice(0, 5);
     }
     return targetExerciseList;
   }
