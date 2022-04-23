0e6a650427217c0ac6f9cb113c1e75bc565dfa35
codakcodak <tjwjdgus83@naver.com>
2022-04-23 12:28:53 +0900

    docs : modified by prettier

--- back/src/db/schemas/music.js
+++ back/src/db/schemas/music.js
@@ -1,42 +1,39 @@
-import {Schema,model} from "mongoose"
+import { Schema, model } from 'mongoose';
 
-const MusicSchema=new Schema(
-    {
-        id:{
-            type:String,
-            required:true
-        },
-        title:{
-            type:String,
-            required:true
-        },
-        artists:{
-            type:String,
-            required:true
-        },
-        artists_ids:{
-            type:String,
-            required:true
-        },
-        danceability:{
-            type:Number,
-            required:true
-        },
-        tempo:{
-            type:Number,
-            required:true
-        },
-        year:{
-            type:Number,
-            required:true
-        },
-        energy:{
-            type:Number,
-            required:true
-        }
-    }
-)
-const MusicModel=model("Music",MusicSchema)
+const MusicSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    artists: {
+        type: String,
+        required: true,
+    },
+    artists_ids: {
+        type: String,
+        required: true,
+    },
+    danceability: {
+        type: Number,
+        required: true,
+    },
+    tempo: {
+        type: Number,
+        required: true,
+    },
+    year: {
+        type: Number,
+        required: true,
+    },
+    energy: {
+        type: Number,
+        required: true,
+    },
+});
+const MusicModel = model('Music', MusicSchema);
 
-export {MusicModel}
-
+export { MusicModel };
--- back/src/services/musicService.js
+++ back/src/services/musicService.js
@@ -1,29 +1,59 @@
-import {Music} from "../db"
+import { Music } from '../db';
+import {
+    filterAuth,
+    titleMerge,
+    titleMergeSort,
+    yearMerge,
+    yearMergeSort,
+    randomize,
+    deepCopy,
+    nAuth,
+    returnNarr,
+    modeAuth,
+} from '../utils/music';
 
-class musicService{
-    static async getMusics(){
-        const musics=await Music.findAll()
+class musicService {
+    static async getMusics() {
+        const musics = await Music.findAll();
 
         if (!musics) {
-        const errorMessage =
-            "노래가 비어있습니다.";
-        return { errorMessage };
+            const errorMessage = '노래가 비어있습니다.';
+            return { errorMessage };
         }
-        
-        const resultMusics=musics.map((music)=>{
-            const {id,title,artists,artists_ids}=music
+
+        const resultMusics = musics.map((music) => {
+            const { id, title, artists, artists_ids } = music;
             return {
                 id,
                 title,
                 artists,
-                artists_ids
-            }
-        })
-        return {musics:resultMusics }
+                artists_ids,
+            };
+        });
+        return { musics: resultMusics };
     }
 
-    static async getMusicsBy({filter,mode}){
-        // const filteredmusics=await 
+    static async getMusicsBy({ filter, mode, limit }) {
+        limit = Math.ceil(limit);
+        if (!filterAuth(filter)) {
+            const errorMessage =
+                'filter의 설정값이 잘못되었거나 필수 설정값들이 없습니다.';
+            return { errorMessage };
+        }
+
+        if (!modeAuth(mode)) {
+            const errorMessage = '존재하지 않는 mode값입니다.';
+            return { errorMessage };
+        }
+        if (!nAuth(limit)) {
+            const errorMessage = 'limit 값이 유효하지 않습니다.';
+            return { errorMessage };
+        }
+        const filteredMusics = await Music.findByFilter({ filter });
+
+        let result = titleMergeSort(filteredMusics);
+
+        return filteredMusics;
     }
 }
-export {musicService}
+export { musicService };
