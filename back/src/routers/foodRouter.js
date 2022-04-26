1209ad8a3e161468140703612ae20b24438dbaeb
KA <song.kyunga@gmail.com>
2022-04-26 17:35:34 +0900

    refactor: 코드 리뷰 반영, 에러 메시지 추가
    - food.js: index:true 추가
    - Food.js: export 위치 변경
    - foodRouter.js: 빈 body들어올 경우,  errorMessage관리 추가
    - foodService.js: errorMessage관리 추가

--- back/src/routers/foodRouter.js
+++ back/src/routers/foodRouter.js
@@ -1,33 +1,38 @@
+import is from "@sindresorhus/is";
 import { Router } from "express";
 import { foodService } from "../services/foodService";
 
 const foodRouter = Router();
 
-foodRouter.get(
-  "/foods",
-  async function (req, res, next) {
-    try {
-      const foodsCategory = await foodService.getFoods();
-      res.status(200).send(foodsCategory);
-    } catch (error) {
-      next(error);
-    }
+foodRouter.get("/foods", async function (req, res, next) {
+  try {
+    const foodsCategory = await foodService.getFoods();
+    res.status(200).send(foodsCategory);
+  } catch (error) {
+    next(error);
   }
-);
+});
 
-foodRouter.post(
-    "/foods/calories",
-    async function (req, res, next) {
-      try {
-        // 선택한 음식들의 정보가 배열로 들어옴
-        // [{"category":"감자류","volume":100},{"category":"유제품","volume":200}]
-        const foodsInfo = req.body;
-        const calories = await foodService.calculateCalories({ foodsInfo })
-        res.status(200).send({ calories })
-      } catch (error) {
-        next(error);
-      }
+foodRouter.post("/foods/calories", async function (req, res, next) {
+  try {
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "headers의 Content-Type을 application/json으로 설정해주세요"
+      );
     }
-);
+    // 선택한 음식들의 정보가 배열로 들어옴
+    // [{"category":"감자류","volume":100},{"category":"유제품","volume":200}]
+    const foodsInfo = req.body;
+    const calories = await foodService.calculateCalories({ foodsInfo });
+
+    if (calories.errorMessage) {
+      throw new Error(calories.errorMessage);
+    }
+
+    res.status(200).send({ calories });
+  } catch (error) {
+    next(error);
+  }
+});
 
 export { foodRouter };
--- back/src/services/foodService.js
+++ back/src/services/foodService.js
@@ -3,17 +3,28 @@ import { Food } from "../db";
 class foodService {
   static async getFoods() {
     const foods = await Food.findAll();
-    const foodsCategory = foods.map(x => x["category"])
+    const foodsCategory = foods.map((x) => x["category"]);
     return foodsCategory;
   }
 
-  static calculateCalories({ foodsInfo }) {
-    const calories = foodsInfo.reduce(async (acc, cur) => {
-        const sum = await acc
-        const food = await Food.findByCategory({ category: cur["category"] })
-        return sum + food["calories"] * cur["volume"]
-    }, 0)
-    return calories
+  static async calculateCalories({ foodsInfo }) {
+    const calories = await foodsInfo.reduce(async (acc, cur, idx, arr) => {
+      const sum = await acc;
+      const food = await Food.findByCategory({
+        category: cur["category"],
+      });
+      if (!food) {
+        return;
+      }
+      return sum + food["calories"] * cur["volume"];
+    }, 0);
+
+    if (!calories) {
+      const errorMessage = "카테고리를 다시 한 번 확인해 주세요.";
+      return { errorMessage };
+    }
+
+    return calories;
   }
 }
 
