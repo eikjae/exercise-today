f2aa0a1016915729aca1db6d34546aa21a4c03f9
@eikjae <eikjae@gmail.com>
2022-04-27 21:51:22 +0900

    chore: Like MVP 작업 (schema 관련)

---
+++ back/src/db/schemas/like.js
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose";
+
+const LikeSchema = new Schema({
+  user_id: {
+    type: String,
+    required: true,
+  },
+  exercises: {
+    type: Array,
+  },
+  foods: {
+    type: Array,
+  },
+  users: {
+    type: Array,
+  },
+});
+
+const LikeModel = model("Like", LikeSchema);
+
+export { LikeModel };
