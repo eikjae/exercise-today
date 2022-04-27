1ed78a91d3f5f4450f1820dcbebd1ab2368e993c
@eikjae <eikjae@gmail.com>
2022-04-27 21:56:58 +0900

    chore: 데이터 분석 관련 파일들을 저장할 브랜치 내 파일 정리

--- front/src/components/chips/StyledChip.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import BaseChip from "./BaseChip";
-
-export const StyledMainPageChip = styled(BaseChip)`
-  /* margin-bottom: 20px; */
-  /* background-color: white; */
-  /* font-size: 20px; */
-`;
