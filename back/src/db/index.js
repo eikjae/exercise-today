ea9be02f0e164c671d27d1b769756a90ddffebe3
@eikjae <eikjae@gmail.com>
2022-04-28 15:27:07 +0900

    chore: index.js에 Like MVP 관련 import, export 추가

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -3,6 +3,7 @@ import { User } from "./models/User";
 import { Exercise } from "./models/Exercise";
 import { Food } from "./models/Food";
 import { TargetExercise } from "./models/TargetExercise";
+import { Like } from "./models/Like"
 
 const DB_URL =
   process.env.MONGODB_URL ||
@@ -18,4 +19,4 @@ db.on("error", (error) =>
   console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
 );
 
-export { User, Exercise, TargetExercise, Food };
+export { User, Exercise, TargetExercise, Food, Like };
