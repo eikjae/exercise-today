0b5c25e26813119dc28667552a92e21ba3fa74b0
@eikjae <eikjae@gmail.com>
2022-04-28 15:24:49 +0900

    chore: /like/create, /like/exercise 관련 service 작업 완료

--- back/src/services/likeService.js
+++ back/src/services/likeService.js
@@ -1,39 +1,10 @@
 import { Like } from "../db";
 
 class likeService {
-  static async setLike({ user_id, toUpdate }) {
-    let likeInfo = await Like.findByUserId({ user_id });
-
-    if (!likeInfo) {
-      const errorMessage =
-        "정보가 없습니다. user_id 값을 다시 한 번 확인해 주세요.";
-      return { errorMessage };
-    }
-
-    const fieldToUpdate = "exercise";
-    const exercisesInfo = likeInfo.exercise;
-    const newValue = {};
-    if (toUpdate.exercise in exercisesInfo) {
-      const d = exercisesInfo.length;
-      for (let i = 0; i < d; i++) {
-        if (exercisesInfo[i] == toUpdate.exercise) {
-          exercisesInfo.splice(i, 1);
-          break;
-        }
-      }
-      newValue = exercisesInfo;
-    } else {
-      exercisesInfo = exercisesInfo.push(toUpdate.exercise);
-    }
-    newValue = exercisesInfo;
-    user = await User.update({ user_id, fieldToUpdate, newValue });
-
-    return user;
-  }
 
   static async addLike({ user_id }) {
-    const isExistlikeInfo = await Like.findByUserId({ user_id });
-    if (isExistlikeInfo) {
+    const existLikeInfo = await Like.findByUserId({ user_id });
+    if (existLikeInfo) {
       const errorMessage =
         "user_id에 대한 likeInfo가 이미 존재합니다.";
       return { errorMessage };
@@ -44,6 +15,38 @@ class likeService {
 
     return likeInfo;
   }
+  
+  static async setLikeExercise({ user_id, toUpdate }) {
+    let likeInfo = await Like.findByUserId({ user_id });
+
+    if (!likeInfo) {
+      const errorMessage =
+        "정보가 없습니다. user_id 값을 다시 한 번 확인해 주세요.";
+      return { errorMessage };
+    }
+
+    const fieldToUpdate = "exercises";
+    let exercisesInfo = likeInfo.exercises;
+    let newValue = {};
+    if (exercisesInfo.includes(toUpdate.exercise)) {
+      const d = exercisesInfo.length;
+      for (let i = 0; i < d; i++) {
+        if (exercisesInfo[i] == toUpdate.exercise) {
+          exercisesInfo.splice(i, 1);
+          break;
+        }
+      }
+      newValue = exercisesInfo;
+    } else {
+      exercisesInfo.push(toUpdate.exercise);
+    }
+    newValue = exercisesInfo;
+    console.log("newValue:", newValue);
+    const updatedLike = await Like.update({ user_id, fieldToUpdate, newValue });
+
+    return updatedLike;
+  }
+
 }
 
 export { likeService };
