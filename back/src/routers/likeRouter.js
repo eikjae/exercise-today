a9975d970e05d27d9014b137cd5dd39825d39095
@eikjae <eikjae@gmail.com>
2022-04-30 16:58:09 +0900

    chore: GET like router 작업 완료

--- back/src/routers/likeRouter.js
+++ back/src/routers/likeRouter.js
@@ -177,4 +177,20 @@ likeRouter.get("/like/music", login_required, async function (req, res, next) {
   }
 });
 
+likeRouter.get("/like", login_required, async function (req, res, next) {
+  try {
+    const user_id = req.currentUserId;
+
+    const LikeInfo = await likeService.getLike({ user_id });
+
+    if (LikeInfo.errorMessage) {
+      throw new Error(LikeInfo.errorMessage);
+    }
+
+    res.status(200).json(LikeInfo);
+  } catch (error) {
+    next(error);
+  }
+});
+
 export { likeRouter };
