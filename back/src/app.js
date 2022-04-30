6bf45923d975d25d0a25230405b8637e3067060f
codakcodak <tjwjdgus83@naver.com>
2022-04-30 21:41:48 +0900

    docs : mofied some router  position

--- back/src/app.js
+++ back/src/app.js
@@ -30,8 +30,8 @@ app.get("/", (req, res) => {
 });
 
 // router, service 구현 (userAuthRouter는 맨 위에 있어야 함.)
-app.use(oauthRouter);
 app.use(userAuthRouter);
+
 app.use(exerciseRouter);
 app.use(foodRouter);
 app.use(targetExerciseRouter);
@@ -41,6 +41,7 @@ app.use(dietRouter);
 app.use(workoutRouter);
 app.use(attendanceRouter);
 app.use(friendRouter);
+app.use(oauthRouter);
 
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
 app.use(errorMiddleware);
