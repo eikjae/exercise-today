ec8dc12ca9c0df617e9d349132bb3f842b0d1cc5
@eikjae <eikjae@gmail.com>
2022-05-03 03:25:12 +0900

    chore: GET mypage/weight/all model 작업 완료

--- back/src/db/models/MyPage.js
+++ back/src/db/models/MyPage.js
@@ -1,9 +1,23 @@
 import { AttendanceModel } from "../schemas/attendance";
+import { todayDate, lastWeek } from "../../utils/date-calculator.js";
 
 export class MyPage {
   static async findByUserId({ userId }) {
     const attendance = await AttendanceModel.find(
       { userId },
+      "whenDate weight"
+    );
+    return attendance;
+  }
+
+  static async findByUserIdWeek({ userId }) {
+    const attendance = await AttendanceModel.find(
+      {
+        $and: [
+          { userId },
+          { whenDate: { $gte: lastWeek(), $lte: todayDate() } },
+        ],
+      },
       "whenDate weight"
     );
     return attendance;
