832624bfd7183f631a359e6bb6ccfafec03a447c
@eikjae <eikjae@gmail.com>
2022-05-03 03:25:53 +0900

    chore: 오늘 날짜 관련 함수 추가

--- back/src/utils/date-calculator.js
+++ back/src/utils/date-calculator.js
@@ -1,4 +1,14 @@
-function getDateStr(myDate) {
+export function todayDate() {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = ("0" + (today.getMonth() + 1)).slice(-2);
+  const date = ("0" + today.getDate()).slice(-2);
+  const whenDate = `${year}-${month}-${date}`;
+
+  return whenDate;
+}
+
+export function getDateStr(myDate) {
   let year = myDate.getFullYear();
   let month = myDate.getMonth() + 1;
   let day = myDate.getDate();
@@ -13,5 +23,6 @@ export function lastWeek() {
   let d = new Date();
   let dayOfMonth = d.getDate();
   d.setDate(dayOfMonth - 7);
+
   return getDateStr(d);
 }
