f40fbafbb2820b2d4be92bfcf017974dd5f58d30
@eikjae <eikjae@gmail.com>
2022-05-04 17:30:10 +0900

    chore: GET /mypage/workout/threemonth router 작업 완료

--- back/src/routers/myPageRouter.js
+++ back/src/routers/myPageRouter.js
@@ -259,4 +259,19 @@ myPageRouter.get(
   }
 );
 
+myPageRouter.get(
+  "/mypage/workout/threemonth",
+  login_required,
+  async function (req, res, next) {
+    try {
+      const userId = req.currentUserId;
+      const workoutInfo = await myPageService.getWorkoutThreeMonth({ userId });
+
+      res.status(200).send(workoutInfo);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 export { myPageRouter };
