68633a7e7357278fcacd0a19a6f4dedbbd01705c
@eikjae <eikjae@gmail.com>
2022-05-04 17:17:41 +0900

    chore: GET /mypage/workout/all router 작업 완료

--- back/src/routers/myPageRouter.js
+++ back/src/routers/myPageRouter.js
@@ -214,4 +214,19 @@ myPageRouter.get(
   }
 );
 
+myPageRouter.get(
+  "/mypage/workout/all",
+  login_required,
+  async function (req, res, next) {
+    try {
+      const userId = req.currentUserId;
+      const workoutInfo = await myPageService.getWorkout({ userId });
+
+      res.status(200).send(workoutInfo);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 export { myPageRouter };
