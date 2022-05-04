a5d114284dc88e3898901cd3e9b428ed8a4af360
@eikjae <eikjae@gmail.com>
2022-05-04 17:43:12 +0900

    chore: GET /mypage/workout/year model 작업 완료

--- back/src/db/models/MyPage.js
+++ back/src/db/models/MyPage.js
@@ -383,4 +383,28 @@ export class MyPage {
 
     return ansList;
   }
+
+  static async findWorkoutYearByUserId({ userId }) {
+    const dietInfo = await CalendarModel.find(
+      {
+        $and: [
+          { userId },
+          {
+            whenDate: {
+              $gte: lastYear(),
+              $lte: todayDate(),
+            },
+          },
+        ],
+      },
+      "whenDate calories.type calories.calorie"
+    );
+    const ansList = dietInfo.map((obj) => {
+      const whenDate = obj.whenDate;
+      const calorie = obj.calories[3].calorie;
+      return { whenDate, calorie };
+    });
+
+    return ansList;
+  }
 }
