f402b2811914528e3eb82a5cb7ccf70c6689869b
@eikjae <eikjae@gmail.com>
2022-05-04 18:25:54 +0900

    chore: GET mypage/calorie/week model 작업 완료

--- back/src/db/models/MyPage.js
+++ back/src/db/models/MyPage.js
@@ -455,4 +455,32 @@ export class MyPage {
 
     return ansList;
   }
+
+  static async findCalorieWeekByUserId({ userId }) {
+    const calorieInfo = await CalendarModel.find(
+      {
+        $and: [
+          { userId },
+          {
+            whenDate: {
+              $gte: lastWeek(),
+              $lte: todayDate(),
+            },
+          },
+        ],
+      },
+      "whenDate calories.type calories.calorie"
+    );
+    const ansList = calorieInfo.map((obj) => {
+      const whenDate = obj.whenDate;
+      const calorie =
+        obj.calories[0].calorie +
+        obj.calories[1].calorie +
+        obj.calories[2].calorie -
+        obj.calories[3].calorie;
+      return { whenDate, calorie };
+    });
+
+    return ansList;
+  }
 }
