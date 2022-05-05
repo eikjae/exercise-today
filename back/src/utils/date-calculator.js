5f81928e6cb7449ad5195f55b59269c56ac22289
@eikjae <eikjae@gmail.com>
2022-05-05 11:49:46 +0900

    refact: 날짜 계산기 리펙토링 작업 완료 (moment 라이브러리)

--- back/src/utils/date-calculator.js
+++ back/src/utils/date-calculator.js
@@ -1,50 +1,31 @@
-const d = new Date();
-const year = d.getFullYear(); // 년
-const month = d.getMonth(); // 월
-const day = d.getDate(); // 일
-
-function rewrite(whenDate) {
-  const year = whenDate.getFullYear();
-  const month = whenDate.getMonth() + 1;
-  const date = whenDate.getDate();
-
-  return `${year}-${month >= 10 ? month : "0" + month}-${
-    date >= 10 ? date : "0" + date
-  }`;
-}
+import moment from "moment";
 
 // 오늘 날짜 구하기
 export function todayDate() {
-  let whenDate = new Date(year, month, day);
-  return rewrite(whenDate);
+  return moment().format("YYYY-MM-DD");
 }
 
 // 일주일 전 구하기
 export function lastWeek() {
-  let whenDate = new Date(year, month, day - 7);
-  return rewrite(whenDate);
+  return moment().subtract(7, "days").format("YYYY-MM-DD");
 }
 
 // 1개월 전 구하기
 export function lastMonth() {
-  let whenDate = new Date(year, month - 1, day);
-  return rewrite(whenDate);
+  return moment().subtract(1, "month").format("YYYY-MM-DD");
 }
 
 // 3개월 전 구하기
 export function lastThreeMonth() {
-  let whenDate = new Date(year, month - 3, day);
-  return rewrite(whenDate);
+  return moment().subtract(3, "month").format("YYYY-MM-DD");
 }
 
 // 6개월 전 구하기
 export function lastSixMonth() {
-  let whenDate = new Date(year, month - 6, day);
-  return rewrite(whenDate);
+  return moment().subtract(6, "month").format("YYYY-MM-DD");
 }
 
 // 1년 전 구하기
 export function lastYear() {
-  let whenDate = new Date(year - 1, month, day);
-  return rewrite(whenDate);
+  return moment().subtract(1, "year").format("YYYY-MM-DD");
 }
