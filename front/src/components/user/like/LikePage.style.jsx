6e69c918180960d778497cea046904db15d7944b
[4기-1반] 이예원 <panggun503@gmail.com>
2022-05-06 12:39:14 +0900

    feat: myPage를 없애고 북마크 페이지에 UserCard 추가

--- front/src/components/user/like/LikePage.style.jsx
+++ front/src/components/user/like/LikePage.style.jsx
@@ -1,13 +1,31 @@
 import styled from "styled-components";
 import { styled as styledMui } from "@mui/material/styles";
-import { Container, Tabs, Tab } from "@mui/material";
+import { Container, Tabs, Grid } from "@mui/material";
 
 export const Layout = styled(Container)`
   margin-top: 10px;
-  /* display: flex; */
-  /* flex-direction: row; */
+  display: flex;
+  flex-direction: row;
   max-width: 100%;
-  /* text-align: center; */
+  text-align: center;
+`;
+
+export const LeftRowGrid = styled(Grid)`
+  /* margin-left: 120px; */
+  margin-top: 37px;
+  width: 20%;
+`;
+
+export const ColGrid = styled(Grid)`
+  flex-direction: column;
+  margin-bottom: 50px;
+`;
+
+export const RightRowGrid = styled(Grid)`
+  width: 100%;
+  height: 100%;
+  margin-left: 10px;
+  /* margin-right: 100px; */
 `;
 
 export const UserName = styled.h2`
